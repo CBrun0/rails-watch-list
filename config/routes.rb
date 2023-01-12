74f730c2a97d796393c78027078620cace7b4e4e
bruno chehboun <bruno.chehboun@gmail.com>
2023-01-12 19:59:11 +0100

    Ex-watch list heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,28 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
+
+# [:index, :new, :create, :show, :edit, :update, destroy ]
+
+# Rails.application.routes.draw do
+#   # Read all
+#   get    "movies", to: "movies#index"
+#   # Create
+#   get    "lists/new", to: "lists#new",  as: :new_movie
+#   post   "lists",     to: "lists#create"
+#   # Read one - The `show` route needs to be *after* `new` route.
+#   get    "movies/:id",      to: "movies#show", as: :movie
+#   # Update
+#   get    "bookmarks/:id/edit", to: "bookmarks#edit", as: :edit_movie
+#   patch  "bookmarks/:id",      to: "bookmarks#update"
+#   # Delete
+#   delete "bookmarks/:id",      to: "bookmarks#destroy"
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  movies = Movie.create!(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    # poster_url: Faker::Movie.image,
+    rating: rand(0..10)
+  )
+end
